ff6aad38217348433810160ae5399887ecc3c6f0
gavinzbq <shanyun@g.clemson.edu>
2017-04-05 11:29:24 -0400

    shell script to run the code

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# I'll execute my programs, with the input directory log_input and output the files in the directory log_output
+python ./src/process_log.py process ./log_input/log.txt 
